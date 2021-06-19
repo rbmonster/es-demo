09c9cbee95361222edc59f8140ff7e6211a61596
rbmonster <srdestiny@163.com>
2021-06-20 00:11:33 +0800

    testdata

---
+++ src/main/java/com/example/demo/repository/TradeContractRepository.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.TestEntity;
+import com.example.demo.entity.TradeContract;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <pre>
+ * @Description:
+ *
+ * </pre>
+ *
+ * @version v1.0
+ * @ClassName: TradeContractRepository
+ * @Author: sanwu
+ * @Date: 2021/6/20 0:01
+ */
+
+@Repository
+public interface TradeContractRepository extends ElasticsearchRepository<TradeContract, String> {
+
+    @Query("{\"match\": {\"status\": {\"query\": \"?0\"}}}")
+    Page<TradeContract> findByName(String status, Pageable pageable);
+
+}
